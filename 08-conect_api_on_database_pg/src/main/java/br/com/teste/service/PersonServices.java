aad891499da602b76f2a49c09fcdf19558e2caa1
raphaeldeoliveira <rafaolive3003@gmail.com>
2023-09-28 22:41:42 -0300

    conclusao do projeto da primeira api funcional, onde eu consigo fazer um crud em uma base de dados. Todos os endpoints foram consumidos corretamente através do postman

--- 08-conect_api_on_database_pg/src/main/java/br/com/teste/service/PersonServices.java
+++ 08-conect_api_on_database_pg/src/main/java/br/com/teste/service/PersonServices.java
@@ -1,20 +1,18 @@
 package br.com.teste.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.teste.exceptions.ResourceNotFoundException;
 import br.com.teste.model.Person;
 import br.com.teste.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
 
-	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
 	@Autowired
@@ -22,48 +20,45 @@ public class PersonServices {
 	
 	public List<Person> findAll() {
 		logger.info("Finding all people");
-		List<Person> persons = new ArrayList<>();
-		for (int i=0; i<8; i++) {
-			Person person = mockPerson(i);
-			persons.add(person);
-		}
-		return persons;
+
+		return repository.findAll();
 	}
 	
-	public Person findById(String id) {
+	public Person findById(Long id) {
 		logger.info("Finding one person!");
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Raphael");
-		person.setLastName("Carvalho");
-		person.setAddress("Lages - SC - BR");
-		person.setGender("Male");
-		return person;
+		
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
 	}
 	
 	public Person create(Person person) {
 		logger.info("Creating one person!");
-		return person;
+		return repository.save(person);
 	}
 	
 	public Person update(Person person) {
 		logger.info("Updating one person!");
-		return person;
+		
+		// primeiro recuperamos o person depois atualizamos os dados e salvamos
+		// assim não sobrescrevemos o objeto
+		var entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(person);
 	}
 	
-	public void delete(String id) {
+	public void delete(Long id) {
 		logger.info("Deleting one person!");
-	}
-	
-	private Person mockPerson(int i) {
-
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Person name"+i);
-		person.setLastName("Last name"+i);
-		person.setAddress("Some address in Brasil"+i);
-		person.setGender("Male");
-		return person;
+		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		repository.delete(entity);
 	}
 	
 }
