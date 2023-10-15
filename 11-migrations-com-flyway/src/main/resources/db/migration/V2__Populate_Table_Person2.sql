00cbc3272c3b5b14be151ba9f4a1b953384710b3
raphaeldeoliveira <rafaolive3003@gmail.com>
2023-10-15 10:53:40 -0300

    Adicionei migrations ao projeto com o uso do flyway

---
+++ 11-migrations-com-flyway/src/main/resources/db/migration/V2__Populate_Table_Person2.sql
@@ -0,0 +1,13 @@
+
+--
+-- Data for Name: person; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.person (id, address, first_name, gender, last_name) FROM stdin;
+4	Bikini city	Bob	Male	Esponja
+6	RS	Gaucho	Male	da Fronteira
+7	Lages	Gilita	Muié	Minha pepi
+8	quintal	Luma	gata	nada
+9	casa da gita	Piçudo	maxoco	Rathael
+10	no fundo dos meu sonhos, o meu grande amoooooorrrrr	Leonardo	sertan	Sonhador
+\.
